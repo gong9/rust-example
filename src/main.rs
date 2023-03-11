3713dc19030d45a002052c151906f78dd5f67c7b
gong <517441540@qq.com>
2023-03-11 15:15:16 +0800

    feat: add guess the number

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("guess the number!");
+
+    let rand_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&rand_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
